b5db85b727cb60721bf6b007711e5e41530a6f79
Michael Caveney <dylanesque27@gmail.com>
2019-03-15 21:26:00 -0400

    Added basic instructions and base styles to prototype page layout

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div id="drum-machine">
-      <p id="display"></p>
+      <p id="display">I am the display!</p>
       <div className="drum-pad">Q</div>
       <div className="drum-pad">W</div>
       <div className="drum-pad">E</div>
@@ -20,5 +20,20 @@ class App extends Component {
     );
   }
 }
+
+//  Within each .drum-pad, there should be an HTML5 audio element which has a src attribute
+// pointing to an audio clip, a class name of clip, and an id corresponding to the inner
+// text of its parent .drum-pad (e.g. id="Q", id="W", id="E" etc.).
+
+// When I click on a .drum-pad element, the audio clip contained in its child audio element should be triggered.
+
+// When I press the trigger key associated with each .drum-pad, the audio clip contained in
+// its child audio element should be triggered (e.g. pressing the Q key should trigger the
+// drum pad which contains the string "Q", pressing the W key should trigger the drum pad
+// which contains the string "W", etc.).
+
+// When a .drum-pad is triggered, a string describing the associated audio clip is displayed
+// as the inner text of the #display element (each string must be unique).
+
 
 export default App;
